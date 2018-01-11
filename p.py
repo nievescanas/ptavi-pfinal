222d977c2a18adf90b5e49d7250632b4052aa81b
nievescanas <nievescanas@github.com>
2018-01-11 20:04:27 +0100

    Elimino archivos innesarios

--- p.py
+++
@@ -1,9 +0,0 @@
-
-import xml.etree.ElementTree as ET
-
-xml_dicc = {}
-tree = ET.parse('ua1.xml')
-root = tree.getroot()
-for child in root:
-    xml_dicc[str(child.tag)] = child.attrib
-print(self.client_address)
